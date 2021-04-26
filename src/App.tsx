cc14558f9520ed38471f3c29c3863bd4f11324d9
Ivan <i.zhuravlev@conteq.ru>
2021-04-26 16:19:36 +0300

    fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import PostsList from "./components/PostsList"
-
-
-const posts: IPosts[] = [
-  {userId: "1", id: "1", title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit", body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"}
-]
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import PostsList from "./components/PostsList";
+import { IPost } from "./types/types";
 
 const App = () => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    try {
+      const response = await axios.get<IPost>(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setPosts(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  }
+
   return (
     <div>
       <PostsList posts={posts} />
